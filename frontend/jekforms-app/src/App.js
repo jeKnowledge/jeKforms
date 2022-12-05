a6ecf1f754ce1814daffdc92249824625714f913
Pedro Nuno Monteiro <pedropessoamonteiro@gmail.com>
2022-12-05 00:36:13 +0000

    Minor updates on home page and axios

--- frontend/jekforms-app/src/App.js
+++ frontend/jekforms-app/src/App.js
@@ -1,19 +1,34 @@
 import './App.css';
 
 import Links from './Links';
-import React from 'react';
+import React, { useState } from 'react';
 import { GoogleLogin } from 'react-google-login';
 import ReactDOM from 'react-dom';
 import { useEffect } from 'react';
+import axios from 'axios'
 
 //const clientId = "926589748534-haun8k580htm3scicreql3a1gsh3f2ta.apps.googleusercontent.com";
 
 function App() {
 
+  const [forms, setForms] = useState([])
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/home/forms/`")
+    .then(res => {
+      console.log(res);
+      setForms(res.data);
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+  }, [])
+
   function handleCallbackResponse(response) {
     console.log(response)
     const token = response.credential
-    axios.post("")
+    //axios.post("")
   }
   
   useEffect(() => {
@@ -36,9 +51,9 @@ function App() {
 
   return (
     <div>
-      <div className='header'>
         <div className='header-login'>
-         <div className='signIn' id="signInDiv"></div>
+         <div className='signIn' id="signInDiv">
+         </div>
           {/*
             <GoogleLogin
             clientId="926589748534-haun8k580htm3scicreql3a1gsh3f2ta.apps.googleusercontent.com"
@@ -51,18 +66,16 @@ function App() {
             cookiePolicy={'single_host_origin'}
             /> */}
         </div>
-      </div>
       <div className='jeKlogo'>
-        <img src="icon.svg" width={216} height={218}/>
+        <img src="./icon.svg" alt="jeKlogo" width={216} height={218}/>
       </div>
       <div className='text'>
         jeKForms
       </div>
       <div className='buttons'>
-        <Links forms_name={"Recrutamento"} forms_url={"https://docs.google.com/forms/u/0/?tgif=d"}></Links>
-        <Links forms_name={"jeKacademy"} forms_url={"https://docs.google.com/forms/u/0/?tgif=d"}></Links>
-        <Links forms_name={"Pizza night"} forms_url={"https://docs.google.com/forms/u/0/?tgif=d"}></Links>
-        <Links forms_name={"Tech"} forms_url={"https://docs.google.com/forms/u/0/?tgif=d"}></Links>
+        {forms.map((form, index) =>
+          <Links key={index} form={form} />
+        )}
       </div>
     </div> 
   );
